27c54ac45c85eb962fb89ca1b42dab4f09b5b604
Santam Roy Choudhury <santamdev404@gmail.com>
2021-02-12 01:18:00 +0530

    'First'

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 
 const publicPath = path.join(__dirname,'client', 'build');
-const port = process.env.PORT ;
+const port = process.env.PORT || 4000;
 app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
+if(process.env.NODE_ENV=="production"){
+
+}
+
 app.listen(port, () => {
    console.log('Server is up on port: '+port);
 });
