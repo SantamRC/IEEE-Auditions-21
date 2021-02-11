6db21d94080f801ac93fa9a45d0c93f94b994717
Santam Roy Choudhury <santamdev404@gmail.com>
2021-02-11 23:53:57 +0530

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const publicPath = path.join(__dirname,'client', 'build');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
