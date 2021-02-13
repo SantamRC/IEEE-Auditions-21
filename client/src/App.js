cf67b9c9e0a834d63b9b7b180f21a262df25b228
Santam Roy Choudhury <santamdev404@gmail.com>
2021-02-13 18:48:20 +0530

    'db'

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import Form from './Components/Form'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-  
-      <Form />
+      <BrowserRouter>
+      <div>
+        
+      </div>
+        <Form />
+      </BrowserRouter>
+      
 
     </div>
   );
