bfd03d1af1f436aebf65dab513427fe9137e2ed1
Santam Roy Choudhury <santamdev404@gmail.com>
2021-02-16 21:52:53 +0530

    'new'

--- client/src/Components/homepage.jsx
+++ client/src/Components/homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import video from '../Assets/tech.mp4'
 import './homepage.css'
 import MediaQuery from 'react-responsive'
-import videoM from '../Assets/tech-mobile.mp4'
+import videoM from '../Assets/tech_mobile.mp4'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
