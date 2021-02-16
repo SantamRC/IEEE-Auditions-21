281f4ef21b4a35d497834e845dd19a66dcf45d65
Santam Roy Choudhury <santamdev404@gmail.com>
2021-02-16 15:33:08 +0530

    'new'

--- client/src/Components/home.jsx
+++ client/src/Components/home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {GoogleLogin} from 'react-google-login'
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from "react-router"
+import './home.css'
 
 const clientID="797040142042-snt599hegqc8tr57v02ha8hpvd9rdek5.apps.googleusercontent.com";
 const clientID2="797040142042-0mj9t3tdd2e5dte3g1n04fg7a9r1slm5.apps.googleusercontent.com"
@@ -31,6 +32,62 @@ export default function Home(props){
     }
 
     return(
+        <div>
+        <div class="container">
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://www.ieeesbnitdgp.com/" target="_blank" >IEEESB </a>
+                    </li>
+                    <li>
+                        <a href="#" > Contact </a>
+                    </li>
+                    <li>
+                        <a href="https://insight.ieeesbnitdgp.com/" target="_blank"> Insight </a>
+                    </li>
+                    <li>
+                        <a href="#btn"> Register </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+
+
+
+
+    <div class="logo"><b>I<span>EE</span>E<span>SB</span> Auditions</b></div>
+
+    </div>
+
+    <div class="container2">
+        <a href="https://ieeeauditions21.herokuapp.com/form" class="zind">
+            <button id="btn" onCl>
+                Register Now
+            </button>
+        </a>
+
+        <p>
+            Come Join Us  <br/>
+        </p>
+
+        <div class="container3">
+            <div class="chevron"></div>
+            <div class="chevron"></div>
+            <div class="chevron"></div>
+            <span class="text">Scroll down</span>
+        </div>
+
+    </div>
+
+
+    <div class="container4">
+        <div class="wpic">
+            Why Join Us ?
+        </div>
+    </div>
+
         <GoogleLogin 
         className={classes.signin}
         clientId={clientID2}
@@ -39,5 +96,6 @@ export default function Home(props){
         onFailure={onFailure}
         isSignedIn={false}
         />
+        </div>
     )
 }
